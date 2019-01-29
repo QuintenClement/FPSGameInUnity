45665c09ffaefcd2877e69184f930baa605706a7
Quinten Clement <quintenclement@gmail.com>
2019-01-29 17:54:06 -0600

    Added enemy model, and animations. Added shooting bullets/spears/arrows functions. Zoom in and out functions for weapons.

---
+++ Assets/Scripts/Weapon Scripts/ArrowBowScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowBowScript : MonoBehaviour
+{
+    private Rigidbody myBody;
+    public float speed = 30f;
+    public float deactivate_Timer = 3f;
+    public float damage = 15f;
+    
+
+
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DeactivateGameObject", deactivate_Timer);
+    }
+   
+
+    public void Launch(Camera mainCamera)
+    {
+        //"shoots" object in the direction the camera is facing
+        myBody.velocity = mainCamera.transform.forward * speed;
+        //rotate object to correct orientation
+        transform.LookAt(transform.position + myBody.velocity);
+    }
+    void DeactivateGameObject()
+    {
+        if(gameObject.activeInHierarchy)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter(Collider target)
+    {
+        //after hits enemy, deactivate game object
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}//class
